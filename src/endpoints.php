7243b89e1513e7bbd9c6b337eeda0d0cfba26ef2
cristiankirk <cristiankirk@gmail.com>
2024-01-09 21:52:37 -0300

    BaseUrl en endpoints.php

--- src/endpoints.php
+++ src/endpoints.php
@@ -1,11 +1,12 @@
 <?php
-$urlPostsSticky = 'http://localhost/conceptoprensa/wordpress/wp-json/wp/v2/posts?sticky=true';
-$urlPostsNoSticky = 'http://localhost/conceptoprensa/wordpress/wp-json/wp/v2/posts?page=1&per_page=3&sticky=false';
-$urlCategorias = 'http://localhost/conceptoprensa/wordpress/wp-json/wp/v2/categories';
-$urlMedia = 'http://localhost/conceptoprensa/wordpress/wp-json/wp/v2/media';
-$urlPosts = 'http://localhost/conceptoprensa/wordpress/wp-json/wp/v2/posts?sticky=false&per_page=3';
-$urlSinglePost = 'http://localhost/conceptoprensa/wordpress/wp-json/wp/v2/posts';
-$urlRelatedTags = 'http://localhost/conceptoprensa/wordpress/wp-json/wp/v2/posts?tags=';
-$urlTags = 'http://localhost/conceptoprensa/wordpress/wp-json/wp/v2/posts';
-$urlPostsCategoria = 'http://localhost/conceptoprensa/wordpress/wp-json/wp/v2/posts?categories=';
+$baseUrl = 'http://localhost/conceptoprensa/wordpress/';
+$urlPostsSticky = $baseUrl.'wp-json/wp/v2/posts?sticky=true';
+$urlPostsNoSticky = $baseUrl.'/wp-json/wp/v2/posts?page=1&per_page=3&sticky=false';
+$urlCategorias = $baseUrl.'wp-json/wp/v2/categories';
+$urlMedia = $baseUrl.'wp-json/wp/v2/media';
+$urlPosts = $baseUrl.'wp-json/wp/v2/posts?sticky=false&per_page=3';
+$urlSinglePost = $baseUrl.'wp-json/wp/v2/posts';
+$urlRelatedTags = $baseUrl.'wp-json/wp/v2/posts?tags=';
+$urlTags = $baseUrl.'wp-json/wp/v2/posts';
+$urlPostsCategoria = $baseUrl.'wp-json/wp/v2/posts?categories=';
 ?>
